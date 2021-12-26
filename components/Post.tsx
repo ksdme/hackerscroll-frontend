dbb35b4710be04e63a8baf9bebf3f6b1e2a56c91
Kilari Teja <kilariteja9@gmail.com>
2021-12-26 23:14:03 +0530

    Update Post

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,6 +1,6 @@
 import { ArrowsExpandIcon } from '@heroicons/react/outline'
 import { CheckCircleIcon } from '@heroicons/react/outline'
-import { UploadIcon } from '@heroicons/react/outline'
+import { ExternalLinkIcon } from '@heroicons/react/outline'
 import Button from '../components/Button'
 
 /*
@@ -12,38 +12,39 @@ export default function Post(props: Props) {
   } = props
 
   return (
-    <div className="flex justify-between items-center p-4 hover:bg-gray-50 cursor-pointer">
-      <div className="flex gap-x-2">
-        <div className="text-gray-400 text-lg">
-          {index}.
+    <div className="flex flex-col p-4 gap-y-4 hover:bg-gray-50">
+      <div className="flex justify-between items-center cursor-pointer">
+        <div className="flex gap-x-2">
+          <div className="text-gray-400 text-lg">
+            {index}.
+          </div>
+
+          <div className="flex flex-col">
+            <h3 className="text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h3>
+
+            <h4 className="text-base text-gray-400">
+              by kilariteja
+            </h4>
+          </div>
         </div>
 
-        <div className="flex flex-col">
-          <h3 className="text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
+        <div className="flex gap-x-4">
+          <Button
+            icon={ArrowsExpandIcon}
+            label="Expand"
+          />
 
-          <h4 className="text-base text-gray-400">
-            by kilariteja
-          </h4>
+          <Button
+            icon={CheckCircleIcon}
+            label="Read"
+          />
+
+          <Button
+            icon={ExternalLinkIcon}
+          />
         </div>
-      </div>
-
-      <div className="flex gap-x-4">
-        <Button
-          icon={ArrowsExpandIcon}
-          label="Expand"
-        />
-
-        <Button
-          icon={CheckCircleIcon}
-          label="Read"
-        />
-
-        <Button
-          icon={UploadIcon}
-          label="Open"
-        />
       </div>
     </div>
   )
