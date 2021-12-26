97b21d4adb6b51f5c30cf50a272c6a4085b864bf
Kilari Teja <kilariteja9@gmail.com>
2021-12-26 21:50:27 +0530

    Add Tailwind

---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
+
+export default class AppDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    return {
+      ...await Document.getInitialProps(ctx),
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@200;300;400;500;600;700&display=swap" />
+        </Head>
+
+        <body className="font-sans">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
