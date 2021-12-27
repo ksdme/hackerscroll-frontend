d4e169f491641fced5f2c0680d68ca60d0d841e0
Kilari Teja <kilariteja9@gmail.com>
2021-12-27 17:50:31 +0530

    Add accent underline nav

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,12 +8,14 @@ export default function Layout(props: Props) {
 
   return (
     <div className="flex flex-col">
-      <nav className="sticky top-0 flex justify-center py-4 z-10 border-b border-gray-200 backdrop-blur-sm bg-white/60">
-        <div className="container">
+      <nav className="sticky top-0 z-10 drop-shadow backdrop-blur-sm bg-white/60">
+        <div className="container py-4 mx-auto">
           <div className="font-medium pl-4 md:pl-0">
             HackerScroll
           </div>
         </div>
+
+        <div className="w-full h-0.5 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500" />
       </nav>
 
       <main className="container mx-auto py-8">
