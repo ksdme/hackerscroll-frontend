d2f1bde9c5401e4e4ada3b7cfd618397229a892f
Kilari Teja <kilariteja9@gmail.com>
2021-12-27 23:43:39 +0530

    Add Default Content component

---
+++ components/DefaultContent.tsx
@@ -0,0 +1,33 @@
+import { useMemo } from 'react'
+import parse from 'url-parse'
+import { Post } from '../models/Post'
+import { hnItemUrl } from '../utils/urls'
+
+/*
+  The default content renderer.
+*/
+export default function DefaultContent(props: Props) {
+  const {
+    post,
+  } = props
+
+  const url = useMemo(() => {
+    return parse(post.url ?? hnItemUrl(post.hn_id), true)
+  }, [
+    post.url,
+    post.hn_id,
+  ])
+
+  return (
+    <button
+      className="w-full h-44 flex justify-center items-center"
+      onClick={() => window.open(post.url ?? hnItemUrl(post.hn_id))}
+    >
+      {url.host}<span className="text-gray-400">{url.pathname}</span>
+    </button>
+  )
+}
+
+interface Props {
+  post: Post
+}
