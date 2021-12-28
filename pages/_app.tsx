94fbb92c0fc21d95a12232f63408300ca19744ed
Kilari Teja <kilariteja9@gmail.com>
2021-12-29 05:16:41 +0530

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,24 @@
 import '../styles/tailwind.css'
 import { AppProps } from 'next/app'
+import { GoogleAnalytics, usePagesViews } from 'nextjs-google-analytics'
+import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 // Shared query client for react-query.
 const queryClient = new QueryClient()
 
 function App({ Component, pageProps }: AppProps) {
+  // Track route changes.
+  usePagesViews()
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <React.Fragment>
+      <GoogleAnalytics />
+
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </React.Fragment>
   )
 }
 
