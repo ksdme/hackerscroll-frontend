4d5389777b5ee375a941e446353d713bae7ca794
Kilari Teja <kilariteja9@gmail.com>
2021-12-29 05:39:52 +0530

    Add header and footer for desktop

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from 'react'
+
 /*
   Layout shell component for a page.
 */
@@ -7,21 +9,35 @@ export default function Layout(props: Props) {
   } = props
 
   return (
-    <div className="flex flex-col">
-      <nav className="sticky top-0 z-10 drop-shadow-sm backdrop-blur-sm bg-white/60">
-        <div className="container py-4 mx-auto">
-          <div className="font-medium pl-4 md:pl-0">
-            HackerScroll
+    <Fragment>
+      <div className="min-h-screen flex flex-col">
+        <nav className="sticky top-0 z-10 drop-shadow-sm backdrop-blur-sm bg-white/60">
+          <div className="container flex justify-between py-4 px-4 md:px-0 mx-auto">
+            <div className="font-medium md:pl-0">
+              HackerScroll
+            </div>
+
+            <a className="hidden md:block" href="https://github.com/ksdme/hackerscroll-frontend" target="_blank">
+              GitHub
+            </a>
           </div>
+
+          <div className="w-full h-0.5 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500" />
+        </nav>
+
+        <main className="container mx-auto py-8">
+          {children}
+        </main>
+      </div>
+
+      <div className="hidden md:block">
+        <div className="container flex justify-center md:justify-end mx-auto pb-16">
+          <a href="https://twitter.com/ksdme" target="_blank">
+            a <span className="text-blue-600">@ksdme</span> product
+          </a>
         </div>
-
-        <div className="w-full h-0.5 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500" />
-      </nav>
-
-      <main className="container mx-auto py-8">
-        {children}
-      </main>
-    </div>
+      </div>
+    </Fragment>
   )
 }
 
